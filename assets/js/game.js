32957eba130e72974630d52a71135a64d1ec19f4
Hannah Brownson <hannahbrownson18@gmail.com>
2022-06-22 22:03:02 -0400

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -137,44 +137,39 @@ var shop = function() {
     'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
   );
 
-  // use switch case to carry out action
   switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
+    case "REFILL": // new case
+    case "refill":
       if (playerMoney >= 7) {
         window.alert("Refilling player's health by 20 for 7 dollars.");
-
-        // increase health and decrease money
+  
         playerHealth = playerHealth + 20;
         playerMoney = playerMoney - 7;
-    }
-    else {
+      }
+      else {
         window.alert("You don't have enough money!");
-    }
+      }
+  
       break;
-    case 'UPGRADE':
-    case 'upgrade':
+    case "UPGRADE": // new case
+    case "upgrade":
       if (playerMoney >= 7) {
         window.alert("Upgrading player's attack by 6 for 7 dollars.");
-
-        // increase attack and decrease money
+  
         playerAttack = playerAttack + 6;
         playerMoney = playerMoney - 7;
-    }
-    else {
+      }
+      else {
         window.alert("You don't have enough money!");
-    }
+      }
+  
       break;
-    case 'LEAVE':
-    case 'leave':
-      window.alert('Leaving the store.');
-
-      // do nothing, so function will end
+    case "LEAVE": // new case
+    case "leave":
+      window.alert("Leaving the store.");
       break;
     default:
-      window.alert('You did not pick a valid option. Try again.');
-
-      // call shop() again to force player to pick a valid option
+      window.alert("You did not pick a valid option. Try again.");
       shop();
       break;
   }
